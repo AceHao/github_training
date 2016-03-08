ef8d7ebb161cd739a382334befda8f9e74b3e4fa
ace_siming_hao <ace.hao@berkeley.edu>
2016-03-07 16:43:19 -0800

    mean function is implemented in stats.py

--- stats.py
+++ stats.py
@@ -3,4 +3,10 @@ This is our brand new stats module
 """
 
 def mean(x):
-    raise NotImplementedError()
+    """
+    Function outputs the mean of list x.
+
+    >>>mean([1,2,3])
+    2
+    """
+    return sum(x)/len(x)
